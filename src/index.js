bc909aefa6b28dad24ba2141d5ee08760fb0c477
Natalia Majkowska <natalia@brainsandbeards.com>
2018-11-21 21:24:46 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-const typeDefs = `
-type Query {
-  dogName: String!
-}
-`;
+const { Prisma } = require('prisma-binding');
 
 const resolvers = {
   Query: {
-    dogName: () => `Tommy the chihuahua`
+    dogName: () => `Tommy the chihuahua`,
+    dogs: (root, args, context, queryInfo) => {
+      return context.db.query.dogs({}, queryInfo);
+    }
+  },
+  Mutation: {
+    dog: (root, args, context, queryInfo) => {
+      return context.db.mutation.createDog(
+        {
+          data: {
+            type: args.type,
+            name: args.name
+          }
+        },
+        queryInfo
+      );
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
-  resolvers
+  typeDefs: 'src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/natalia-majkowska/dogs-service/dev',
+      secret: 'testsecret',
+      debug: true
+    })
+  })
 });
 server.start(() => console.log(`Server address: http://localhost:4000`));
